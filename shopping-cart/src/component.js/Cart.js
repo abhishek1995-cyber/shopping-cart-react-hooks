e006842bf43bc2ba3fce5c47aa3e5e984ade13e0
Abhishek kumar <abhishekzadav@Gmail.com>
2023-02-27 11:40:48 +0530

    css

--- shopping-cart/src/component.js/Cart.js
+++ shopping-cart/src/component.js/Cart.js
@@ -16,7 +16,7 @@ function Cart(props){
         <>
  
             <div className="absolute w-1/4 bg-black top-0.5 right-0.5">
-            <i onClick={handleClose} className="text-white text-3xl fa-solid fa-x"></i>
+            <i onClick={handleClose} className="flex justify-end  cursor-pointer  text-white text-3xl fa-solid fa-x"></i>
         { props.cart &&  props.cart.map((cartItem,i) => {
             totalAmount += cartItem.price;
             return (
@@ -26,21 +26,22 @@ function Cart(props){
                   <img className="w-20" src={"/static/products/" + cartItem.sku + "_1.jpg"} alt='product-img'/>
                     </figure>
                     <div>
-                  <h2 className="main-title">{cartItem.title}</h2>
+                  <h2 className="main-title mt-7">{cartItem.title}</h2>
                   <p>{cartItem.currencyFormat + cartItem.price}</p>
+                  <p>{cartItem.style}</p>
                     </div>
                     <div> 
-                        <i onClick={()=> handleDelete(i)} className="fa-solid fa-trash-can-arrow-up"></i>
+                        <i onClick={()=> handleDelete(i)} className="text-2xl mt-10 hover:bg-red-700 cursor-pointer fa-solid fa-trash-can-arrow-up"></i>
                     </div>
                   </div>
 
                 </>
             )
         })}
-          <div className="text-white">
-                    <h3>Total amount : {totalAmount} </h3>
+          <div className="text-white text-center">
+                    <h3 className="text-xl my-3">Total amount : $ <span className="text-red-400">{totalAmount} </span></h3>
                   </div>
-                  <button className="text-white">Checkout</button>
+                  <button className=" bg-gray-400 text-gray-200 ml-28 mb-5 py-2 px-8 rounded-full">Checkout</button>
         </div>
         </>
     )
--- shopping-cart/src/component.js/Main.js
+++ shopping-cart/src/component.js/Main.js
@@ -14,7 +14,7 @@ function Main(props) {
     <div className="w-3/4 grid  grid-cols-3 ">
       {props.display.map((product, i) => {
         return (
-          <div className="text-center relative leading-9 mb-12">
+          <div className="text-center relative leading-9 mb-12 mt-8">
             <img  src={"/static/products/" + product.sku + "_1.jpg"}     alt='product-img'/>
             <span className="absolute top-7 right-5" >
             <span  className={product.isFreeShipping ? "text-green-400" : "text-red-400"}>
